0f8df629bce82b1bf31e7e025973202282d67317
muzakparov <muzakparov@gmail.com>
2018-06-21 11:08:25 +0300

    added new fancies 10 and 15 no

--- src/actions/socket.js
+++ src/actions/socket.js
@@ -218,18 +218,31 @@ export const establishWebSocketConnection=()=>{
 	    }   
 
 	    
-	    let mo,
-	    	ir_lambi,
+	    let mo,	    
+	    	ir_lambi,//overs=20
+
 		    ir_fancy_1_6,
+		    ir_fancy_1_10,
 		    ir_fancy_1_12,
+		    ir_fancy_1_15,		    
+
 		    ir_fancy_2_6,
-		    ir_fancy_2_12
+		    ir_fancy_2_10,
+		    ir_fancy_2_12,
+		    ir_fancy_2_15
 
 		let ir_lambi_Arr,
+
 		    ir_fancy_1_6_Arr,
+		    ir_fancy_1_10_Arr,
 		    ir_fancy_1_12_Arr,
+		    ir_fancy_1_15_Arr,
+
 		    ir_fancy_2_6_Arr,
-		    ir_fancy_2_12_Arr
+		    ir_fancy_2_10_Arr,
+		    ir_fancy_2_12_Arr,
+		    ir_fancy_2_15_Arr
+
 
 		let homeTeamArr, 
 	    	awayTeamArr
@@ -250,17 +263,33 @@ export const establishWebSocketConnection=()=>{
 		let prevFancy_1_6_status
 		let prevFancy_1_6_team
 
+		let prevFancy_1_10_runs
+		let prevFancy_1_10_status
+		let prevFancy_1_10_team
+
 		let prevFancy_1_12_runs
 		let prevFancy_1_12_status
 		let prevFancy_1_12_team
+
+		let prevFancy_1_15_runs
+		let prevFancy_1_15_status
+		let prevFancy_1_15_team
 
 		let prevFancy_2_6_runs
 		let prevFancy_2_6_status
 		let prevFancy_2_6_team
 
+		let prevFancy_2_10_runs
+		let prevFancy_2_10_status
+		let prevFancy_2_10_team
+
 		let prevFancy_2_12_runs
 		let prevFancy_2_12_status
 		let prevFancy_2_12_team
+
+		let prevFancy_2_15_runs
+		let prevFancy_2_15_status
+		let prevFancy_2_15_team
 
 		let prevHomeTeamRunsArr
 		let prevAwayTeamRunsArr
@@ -283,17 +312,33 @@ export const establishWebSocketConnection=()=>{
 	    let didFancy_1_6_StatusChange
 	    let didFancy_1_6_TeamChange
 
+	    let didFancy_1_10_RunsChange
+	    let didFancy_1_10_StatusChange
+	    let didFancy_1_10_TeamChange
+
 	    let didFancy_1_12_RunsChange
 	    let didFancy_1_12_StatusChange
 	    let didFancy_1_12_TeamChange
+
+	    let didFancy_1_15_RunsChange
+	    let didFancy_1_15_StatusChange
+	    let didFancy_1_15_TeamChange
 
 	    let didFancy_2_6_RunsChange
 	    let didFancy_2_6_StatusChange
 	    let didFancy_2_6_TeamChange
 
+	    let didFancy_2_10_RunsChange
+	    let didFancy_2_10_StatusChange
+	    let didFancy_2_10_TeamChange
+
 	    let didFancy_2_12_RunsChange
 	    let didFancy_2_12_StatusChange
 	    let didFancy_2_12_TeamChange
+
+	    let didFancy_2_15_RunsChange
+	    let didFancy_2_15_StatusChange
+	    let didFancy_2_15_TeamChange
 
 	    let didAnyHomeTeamRunsChange
 		let didAnyAwayTeamRunsChange
@@ -326,14 +371,27 @@ export const establishWebSocketConnection=()=>{
 		    ir_fancy_1_6_Arr   	= data.ir.filter(market =>(market.innings ==1 && 
 		    										       market.overs == 6)
 		    							)
+		    ir_fancy_1_10_Arr   = data.ir.filter(market =>(market.innings ==1 && 
+		    										       market.overs == 10)
+		    							)
 		    ir_fancy_1_12_Arr   = data.ir.filter(market =>(market.innings ==1&& 
 		    										       market.overs == 12)
+		    							)
+		    ir_fancy_1_15_Arr   = data.ir.filter(market =>(market.innings ==1&& 
+		    										       market.overs == 15)
 		    							)
 		    ir_fancy_2_6_Arr    = data.ir.filter(market =>(market.innings ==2 && 
 		    										       market.overs == 6)
 		    							)
+		    ir_fancy_2_10_Arr    = data.ir.filter(market =>(market.innings ==2 && 
+		    										       market.overs == 10)
+		    							)
 		    ir_fancy_2_12_Arr   = data.ir.filter(market =>(market.innings ==2 && 
 		    										       market.overs == 12)
+
+		    							)
+		    ir_fancy_2_15_Arr   = data.ir.filter(market =>(market.innings ==2 && 
+		    										       market.overs == 15)
 		    							)
 
 		    homeTeamArr			=data.batsmen
@@ -343,13 +401,17 @@ export const establishWebSocketConnection=()=>{
 			
 			// console.log('homeTeamArr',homeTeamArr)
 			// debugger;
-		    
+
 		    mo              = data.mo		    
 	    	ir_lambi 		= ir_lambi_Arr[0]
 		    ir_fancy_1_6   	= ir_fancy_1_6_Arr[0]
+		    ir_fancy_1_10   = ir_fancy_1_10_Arr[0]
 		    ir_fancy_1_12   = ir_fancy_1_12_Arr[0]
+		    ir_fancy_1_15   = ir_fancy_1_15_Arr[0]
 		    ir_fancy_2_6    = ir_fancy_2_6_Arr[0]
+		    ir_fancy_2_10   = ir_fancy_2_10_Arr[0]
 		    ir_fancy_2_12   = ir_fancy_2_12_Arr[0]
+		    ir_fancy_2_15   = ir_fancy_2_15_Arr[0]
 		    
 
 	    	didMoWinnerChange=(prevMoWinner !== mo.winner)
@@ -363,17 +425,33 @@ export const establishWebSocketConnection=()=>{
 		    didFancy_1_6_StatusChange=(prevFancy_1_6_status!==ir_fancy_1_6.status)
 		    didFancy_1_6_TeamChange=(prevFancy_1_6_team!==ir_fancy_1_6.team)
 
+		    didFancy_1_10_RunsChange=(prevFancy_1_10_runs!==ir_fancy_1_10.runs)
+		    didFancy_1_10_StatusChange=(prevFancy_1_10_status!==ir_fancy_1_10.status)
+		    didFancy_1_10_TeamChange=(prevFancy_1_10_team!==ir_fancy_1_10.team)
+
 		    didFancy_1_12_RunsChange=(prevFancy_1_12_runs!==ir_fancy_1_12.runs)
 		    didFancy_1_12_StatusChange=(prevFancy_1_12_status!==ir_fancy_1_12.status)
 		    didFancy_1_12_TeamChange=(prevFancy_1_12_team!==ir_fancy_1_12.team)
+
+		    didFancy_1_15_RunsChange=(prevFancy_1_15_runs!==ir_fancy_1_15.runs)
+		    didFancy_1_15_StatusChange=(prevFancy_1_15_status!==ir_fancy_1_15.status)
+		    didFancy_1_15_TeamChange=(prevFancy_1_15_team!==ir_fancy_1_15.team)
 
 		    didFancy_2_6_RunsChange=(prevFancy_2_6_runs!==ir_fancy_2_6.runs)
 		    didFancy_2_6_StatusChange=(prevFancy_2_6_status!==ir_fancy_2_6.status)
 		    didFancy_2_6_TeamChange=(prevFancy_2_6_team!==ir_fancy_2_6.team)
 
+		    didFancy_2_10_RunsChange=(prevFancy_2_10_runs!==ir_fancy_2_10.runs)
+		    didFancy_2_10_StatusChange=(prevFancy_2_10_status!==ir_fancy_2_10.status)
+		    didFancy_2_10_TeamChange=(prevFancy_2_10_team!==ir_fancy_2_10.team)
+
 		    didFancy_2_12_RunsChange=(prevFancy_2_12_runs!==ir_fancy_2_12.runs)
 		    didFancy_2_12_StatusChange=(prevFancy_2_12_status!==ir_fancy_2_12.status)
 		    didFancy_2_12_TeamChange=(prevFancy_2_12_team!==ir_fancy_2_12.team)
+
+		    didFancy_2_15_RunsChange=(prevFancy_2_15_runs!==ir_fancy_2_15.runs)
+		    didFancy_2_15_StatusChange=(prevFancy_2_15_status!==ir_fancy_2_15.status)
+		    didFancy_2_15_TeamChange=(prevFancy_2_15_team!==ir_fancy_2_15.team)
 
 
 		    didAnyHomeTeamRunsChange = homeTeamArr?
@@ -546,17 +624,33 @@ export const establishWebSocketConnection=()=>{
 			prevFancy_1_6_status=ir_fancy_1_6.status
 			prevFancy_1_6_team=ir_fancy_1_6.team
 
+			prevFancy_1_10_runs=ir_fancy_1_10.runs
+			prevFancy_1_10_status=ir_fancy_1_10.status
+			prevFancy_1_10_team=ir_fancy_1_10.team
+
 			prevFancy_1_12_runs=ir_fancy_1_12.runs
 			prevFancy_1_12_status=ir_fancy_1_12.status
 			prevFancy_1_12_team=ir_fancy_1_12.team
+
+			prevFancy_1_15_runs=ir_fancy_1_15.runs
+			prevFancy_1_15_status=ir_fancy_1_15.status
+			prevFancy_1_15_team=ir_fancy_1_15.team
 
 			prevFancy_2_6_runs=ir_fancy_2_6.runs
 			prevFancy_2_6_status=ir_fancy_2_6.status
 			prevFancy_2_6_team=ir_fancy_2_6.team
 
+			prevFancy_2_10_runs=ir_fancy_2_10.runs
+			prevFancy_2_10_status=ir_fancy_2_10.status
+			prevFancy_2_10_team=ir_fancy_2_10.team
+
 			prevFancy_2_12_runs=ir_fancy_2_12.runs
 			prevFancy_2_12_status=ir_fancy_2_12.status
 			prevFancy_2_12_team=ir_fancy_2_12.team
+
+			prevFancy_2_15_runs=ir_fancy_2_15.runs
+			prevFancy_2_15_status=ir_fancy_2_15.status
+			prevFancy_2_15_team=ir_fancy_2_15.team
 
 
 			prevHomeTeamRunsArr = homeTeamArr.map(batsmanEl => batsmanEl.runs)
